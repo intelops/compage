5315f767a1d080f3c5ccb7d68d3d51e6cbc9a73f
Mahendra <mahendra.b@intelops.dev>
2022-08-23 22:35:23 +0530

    added remaining configs

--- ui/src/components/diagram-maker-container.tsx
+++ ui/src/components/diagram-maker-container.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from "react-dom";
 import {
     ConnectorPlacement,
+    ConnectorPlacementType,
+    ContextMenuRenderCallbacks,
     DiagramMaker,
-    EditorMode,
+    DiagramMakerEdge,
+    DiagramMakerNode,
+    DiagramMakerPotentialNode,
     Event,
     Shape,
     VisibleConnectorTypes,
@@ -10,55 +14,36 @@ import {
 } from "diagram-maker";
 import "diagram-maker/dist/diagramMaker.css";
 import React, {useRef} from "react";
+import {
+    createCircularNode,
+    createEdgeContextMenu,
+    createLibraryPanel,
+    createNodeContextMenu,
+    createNodeWithDropdown,
+    createNodeWithInput,
+    createPanelContextMenu,
+    createPluginPanel,
+    createPotentialNode,
+    createRectangularConnectorNode,
+    createRectangularNode,
+    createToolsPanel,
+    createWorkspaceContextMenu
+} from "./utils";
+
+import BoundaryCircularData from './BoundaryCircular/data';
 
 export const DiagramMakerContainer = () => {
     const containerRef = useRef() as any;
     const diagramMakerRef = useRef() as any;
 
     React.useEffect(() => {
-        let shape, connectorPlacement, showArrowhead, plugin
-        let initialData = {
-            editor: {mode: EditorMode.DRAG},
-            workspace: {
-                position: {x: 0, y: 0},
-                scale: 1,
-                canvasSize: {width: 3200, height: 1600},
-                viewContainerSize: {width: window.innerWidth, height: window.innerHeight},
-            }, nodes: {
-                // a: {
-                //     id: "a", diagramMakerData: {
-                //         position: {
-                //             x: 200, y: 100
-                //         }, size: {
-                //             width: 100, height: 100
-                //         }
-                //     }
-                // }
-                node1: {
-                    id: 'node1',
-                    typeId: 'testId-normal',
-                    diagramMakerData: {
-                        position: {x: 200, y: 150},
-                        size: {width: 100, height: 100},
-                    },
-                },
-                node2: {
-                    id: 'node2',
-                    typeId: 'testId-normal',
-                    diagramMakerData: {
-                        position: {x: 400, y: 300},
-                        size: {width: 100, height: 100},
-                    },
-                },
-            }, edges: {
-                edge1: {
-                    id: 'edge1',
-                    src: 'node1',
-                    dest: 'node2',
-                    diagramMakerData: {},
-                },
-            }, panels: {}
-        };
+        // let shape, connectorPlacement, showArrowhead, plugin, edgeBadge
+        let plugin, connectorPlacement
+        let initialData = BoundaryCircularData
+        // let connectorPlacement = ConnectorPlacementType.BOUNDARY
+        let showArrowhead = true
+        let shape = Shape.CIRCLE
+        let edgeBadge = true
         diagramMakerRef.current = new DiagramMaker(containerRef.current, {
             options: {
                 connectorPlacement: connectorPlacement || ConnectorPlacement.LEFT_RIGHT,
@@ -68,15 +53,58 @@ export const DiagramMakerContainer = () => {
                 destroy: (container) => {
                     ReactDOM.unmountComponentAtNode(container);
                 },
-                node: (node, container) => {
-                    ReactDOM.render(<Node
-                        //TODO
-                        // id={node.id}
-                        {...node.diagramMakerData.size}
-                        selected={node.diagramMakerData.selected}
-                    />, container);
-                }, edge: () => {
-                }, panels: {}
+                node: (node: DiagramMakerNode<{}>, container: HTMLElement) => {
+                    if (node.typeId === 'testId-centered') {
+                        return createCircularNode(node, container);
+                    }
+                    if (node.typeId === 'testId-input') {
+                        return createNodeWithInput(node, container);
+                    }
+                    if (node.typeId === 'testId-dropdown') {
+                        return createNodeWithDropdown(node, container);
+                    }
+                    if (connectorPlacement === ConnectorPlacement.BOUNDARY) {
+                        if (shape === Shape.CIRCLE) {
+                            return createCircularNode(node, container);
+                        }
+                        return createRectangularConnectorNode(node, container);
+                    }
+                    return createRectangularNode(node, container);
+                },
+                edge: edgeBadge ? (edge: DiagramMakerEdge<{}>, container: HTMLElement): HTMLElement | undefined => {
+                    if (container.innerHTML === '') {
+                        const element = document.createElement('div');
+                        element.textContent = edge.id.substring(0, 10);
+                        element.classList.add('edgeBadge');
+                        container.appendChild(element);
+                        return element;
+                    }
+                    return undefined;
+                } : undefined,
+                potentialNode: (node: DiagramMakerPotentialNode, container: HTMLElement) => createPotentialNode(node, container),
+                panels: {
+                    library: (panel: any, state: any, container: HTMLElement) => createLibraryPanel(container),
+                    ...(plugin && {
+                        plugin: (
+                            panel: any,
+                            state: any,
+                            container: HTMLElement,
+                        ) => createPluginPanel(container, state),
+                    }),
+                    ...(!plugin && {
+                        tools: (
+                            panel: any,
+                            state: any,
+                            container: HTMLElement,
+                        ) => createToolsPanel(container, () => diagramMakerRef.current),
+                    }),
+                },
+                contextMenu: {
+                    node: (id: string | undefined, container: HTMLElement) => createNodeContextMenu(id, container),
+                    edge: (id: string | undefined, container: HTMLElement) => createEdgeContextMenu(id, container),
+                    panel: (id: string | undefined, container: HTMLElement) => createPanelContextMenu(id, container),
+                    workspace: (container: HTMLElement) => createWorkspaceContextMenu(container),
+                } as ContextMenuRenderCallbacks,
             },
             nodeTypeConfig: {
                 'testId-centered': {
@@ -169,12 +197,12 @@ export function handleTestPluginEvent(event: any, diagramMaker: any) {
     }
 }
 
-function Node({selected, width, height}) {
-    return (<div
-        style={{
-            width, height, background: "white", padding: 8, border: selected ? "1px solid red" : undefined
-        }}
-    >
-        Component
-    </div>);
-}
+// function Node({selected, width, height}) {
+//     return (<div
+//         style={{
+//             width, height, background: "white", padding: 8, border: selected ? "1px solid red" : undefined
+//         }}
+//     >
+//         Component
+//     </div>);
+// }
