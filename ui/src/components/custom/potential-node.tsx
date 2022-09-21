d9e37eded0cc6e09a9d916d008fb67e1cd0ff0bd
Mahendra <mahendra.b@intelops.dev>
2022-09-21 09:59:58 +0530

    changed border to black

--- ui/src/components/custom/potential-node.tsx
+++ ui/src/components/custom/potential-node.tsx
@@ -8,7 +8,7 @@ interface PotentialNodeProps {
 export const PotentialNode = (props: PotentialNodeProps) => {
     let config = getConfig(props.typeId);
     return <React.Fragment>
-        <div style={{border: "1px solid red"}} className={config.classNames + " example-node"}>
+        <div style={{border: "1px solid black"}} className={config.classNames + " example-node"}>
             {props.typeId}
         </div>
     </React.Fragment>;
