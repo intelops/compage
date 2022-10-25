6ece17abc47cae2886b9c5df6a986102cd8606c9
Mahendra <mahendra.b@intelops.dev>
2022-10-25 19:33:11 +0530

    added grpc client code

---
+++ app/grpc/protobufs/project.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+export const generateProject = (client, payload) => {
+    client.GenerateProject(payload, (err, response) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("response received : ", response);
+    });
+}
+
+export const getProjectGrpcClient = () => {
+    const PROTO_PATH = __dirname + '/project.proto';
+    const grpc = require('@grpc/grpc-js');
+    const protoLoader = require('@grpc/proto-loader');
+
+    let packageDefinition = protoLoader.loadSync(
+        PROTO_PATH,
+        {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true
+        }
+    );
+
+    const ProjectService = grpc.loadPackageDefinition(
+        packageDefinition).project.ProjectService;
+
+    return new ProjectService(`localhost:50051`, grpc.credentials.createInsecure());
+}
