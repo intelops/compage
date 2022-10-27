274d9b94eeea3efb17773c7d2b6a789629c5886f
Mahendra <mahendra.b@intelops.dev>
2022-10-27 21:28:17 +0530

    added localstorage in place of map

--- compage/src/routes/store.ts
+++ compage/src/routes/store.ts
@@ -1,3 +1,6 @@
+import {LocalStorage} from 'node-localstorage'
+const localStorage = new LocalStorage("./scratch");
+
 export const setUser = (username: string, token: string) => {
     localStorage.setItem(username, token)
 }
