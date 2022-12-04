a5ba235c76cbd1d28105fd2199a669fa08b5e3ba
Mahendra <mahendra.b@intelops.dev>
2022-12-04 23:37:24 +0530

    added devspace config for ui and ui docker file changes

--- ui/src/service/backend-api.ts
+++ ui/src/service/backend-api.ts
@@ -1,38 +1,28 @@
 import axios from 'axios';
-
-let BackendBaseURL
-
-if (process.env.NODE_ENV === 'development') {
-    // export const
-    BackendBaseURL = 'http://localhost:5000'
-} else {
-    // for container
-    BackendBaseURL = 'http://compageapp:5000/'
-}
+import {config} from "../utils/constants";
 
 const BackendApi = () => {
     return axios.create({
-        baseURL: BackendBaseURL
+        baseURL: config.backend_base_url
     })
 }
 
 export const AuthBackendApi = () => {
     const path = "/auth"
     return axios.create({
-        baseURL: BackendBaseURL + path
+        baseURL: config.backend_base_url + path
     })
 }
 export const GithubBackendApi = () => {
     const path = "/github"
     return axios.create({
-        baseURL: BackendBaseURL + path
+        baseURL: config.backend_base_url + path
     })
 }
 
 export const CompageBackendApi = () => {
     const path = "/compage"
     return axios.create({
-        baseURL: BackendBaseURL + path
+        baseURL: config.backend_base_url + path
     })
 }
-export default BackendBaseURL;
--- ui/src/utils/constants.ts
+++ ui/src/utils/constants.ts
@@ -1,9 +1,32 @@
-//TODO externalise the below keys
-import BackendBaseURL from "../service/backend-api";
+const assert = require('assert').strict
+
+// const configValue : string = process.env.REACT_APP_SOME_CONFIGURATION
+
+console.log("process.env.NODE_ENV: ", process.env.NODE_ENV)
+
+// github config
+const GITHUB_APP_CLIENT_ID = process.env.GITHUB_APP_CLIENT_ID
+assert.ok(GITHUB_APP_CLIENT_ID, 'The "GITHUB_APP_CLIENT_ID" environment variable is required')
+const GITHUB_APP_REDIRECT_URI = process.env.GITHUB_APP_REDIRECT_URI
+assert.ok(GITHUB_APP_REDIRECT_URI, 'The "GITHUB_APP_REDIRECT_URI" environment variable is required')
+// app server config
+const COMPAGE_APP_SERVER_URL = process.env.COMPAGE_APP_SERVER_URL
+assert.ok(COMPAGE_APP_SERVER_URL, 'The "COMPAGE_APP_SERVER_URL" environment variable is required')
+
+let BackendBaseURL
+if (process.env.NODE_ENV === 'development') {
+    // export const
+    BackendBaseURL = 'http://localhost:5000'
+} else {
+    // for container
+    BackendBaseURL = COMPAGE_APP_SERVER_URL
+}
 
 export const config = {
-    client_id: "5f8b8208ffa000316d2d",
-    redirect_uri: "http://localhost:3000/login",
+    // github config
+    client_id: GITHUB_APP_CLIENT_ID,
+    redirect_uri: GITHUB_APP_REDIRECT_URI,
+    backend_base_url: BackendBaseURL,
 
     // authRoutes
     proxy_url_check_token: BackendBaseURL + "/auth/check_token",
