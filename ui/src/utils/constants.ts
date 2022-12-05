4aefdeb5c3d6878b247dc4da0280f00cefc92f83
Mahendra <mahendra.b@intelops.dev>
2022-12-05 14:23:06 +0530

    added react-app-env to create different builds

--- ui/src/utils/constants.ts
+++ ui/src/utils/constants.ts
@@ -13,31 +13,22 @@ assert.ok(REACT_APP_GITHUB_APP_REDIRECT_URI, 'The "REACT_APP_GITHUB_APP_REDIRECT
 const REACT_APP_COMPAGE_APP_SERVER_URL = process.env.REACT_APP_COMPAGE_APP_SERVER_URL
 assert.ok(REACT_APP_COMPAGE_APP_SERVER_URL, 'The "REACT_APP_COMPAGE_APP_SERVER_URL" environment variable is required')
 
-let BackendBaseURL
-if (process.env.NODE_ENV === 'development') {
-    // export const
-    BackendBaseURL = 'http://localhost:5000'
-} else {
-    // for container
-    BackendBaseURL = REACT_APP_COMPAGE_APP_SERVER_URL
-}
-
 export const config = {
     // github config
     client_id: REACT_APP_GITHUB_APP_CLIENT_ID,
     redirect_uri: REACT_APP_GITHUB_APP_REDIRECT_URI,
-    backend_base_url: BackendBaseURL,
+    backend_base_url: REACT_APP_COMPAGE_APP_SERVER_URL,
 
     // authRoutes
-    proxy_url_check_token: BackendBaseURL + "/auth/check_token",
-    proxy_url_logout: BackendBaseURL + "/auth/logout",
+    proxy_url_check_token: REACT_APP_COMPAGE_APP_SERVER_URL + "/auth/check_token",
+    proxy_url_logout: REACT_APP_COMPAGE_APP_SERVER_URL + "/auth/logout",
 
     // compageRoutes
-    proxy_url_create_project: BackendBaseURL + "/compage/create_project",
+    proxy_url_create_project: REACT_APP_COMPAGE_APP_SERVER_URL + "/compage/create_project",
 
     // githubRoutes
-    proxy_url_create_repository: BackendBaseURL + "/github/create_repository",
-    proxy_url_list_repositories: BackendBaseURL + "/github/list_repositories",
-    proxy_url_commit_compage_yaml: BackendBaseURL + "/github/commit_compage_yaml",
-    proxy_url_pull_compage_yaml: BackendBaseURL + "/github/pull_compage_yaml",
+    proxy_url_create_repository: REACT_APP_COMPAGE_APP_SERVER_URL + "/github/create_repository",
+    proxy_url_list_repositories: REACT_APP_COMPAGE_APP_SERVER_URL + "/github/list_repositories",
+    proxy_url_commit_compage_yaml: REACT_APP_COMPAGE_APP_SERVER_URL + "/github/commit_compage_yaml",
+    proxy_url_pull_compage_yaml: REACT_APP_COMPAGE_APP_SERVER_URL + "/github/pull_compage_yaml",
 };
