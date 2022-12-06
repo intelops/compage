af4b2bd823d56035778b71291d5716da6aebfe99
Mahendra <mahendra.b@intelops.dev>
2022-12-06 22:11:48 +0530

    Fixed issue in core where templates were copied. Opened NodePort for app service as well.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,19 @@
-# create kind cluster and install everything on kind cluster
+#######################################################################
+# create kind cluster and install everything on kind cluster          #
+#######################################################################
+
 # creates kind cluster with name compage
 kind create cluster --name compage --config kind-config.yaml
 # creates compage namespace
 kubectl create namespace compage
 # creates redis namespace
 kubectl create namespace redis
+
+#######################################################################
+# Make sure you have created docker images and updated the image tags #
+# in respective deployments                                           #
+#######################################################################
+
 # creates resources for core
 kubectl apply -f ../core/manifests
 # creates resources for app
--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -1,2 +1,12 @@
+#######################################################################
+# Uninstall everything on kind cluster and delete it                  #
+#######################################################################
+
+# deletes resources for ui
+kubectl delete -f ../ui/manifests
+# deletes resources for app
+kubectl delete -f ../app/manifests
+# deletes resources for core
+kubectl delete -f ../core/manifests
 # cleans compage cluster
 kind delete cluster --name compage
