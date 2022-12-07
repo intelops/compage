6a8baafae6ec26b216bc51f43379cf76f43a589c
Mahendra <mahendra.b@intelops.dev>
2022-12-07 19:58:51 +0530

    deploy.sh is working now

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,9 @@
 #######################################################################
 # create kind cluster and install everything on kind cluster          #
 #######################################################################
-
+CLUSTER_NAME=compage
 # creates kind cluster with name compage
-kind create cluster --name compage --config kind-config.yaml
+kind create cluster --name $CLUSTER_NAME --config kind-config.yaml
 # creates compage namespace
 kubectl create namespace compage
 # creates redis namespace
@@ -11,13 +11,19 @@ kubectl create namespace redis
 
 # retrieves nodes ip [tested on single node cluster]
 KIND_NODE_IP=$(kubectl get nodes -o wide --no-headers | awk -v OFS='\t' '{print $6}')
+if [ -z "$KIND_NODE_IP" ]; then
+  echo "KIND_NODE_IP: \$KIND_NODE_IP is empty"
+  exit 0
+fi
+
+# update KIND_NODE_IP in configmaps
 sed -i "s/{{KIND_NODE_IP}}/$KIND_NODE_IP/" ../app/manifests/configmaps.yaml
 sed -i "s/{{KIND_NODE_IP}}/$KIND_NODE_IP/" ../ui/manifests/configmaps.yaml
 
 TAG_NAME="first"
-CORE_IMAGE="ghcr.io/mahendraintelops/compage/core"+$TAG_NAME
-APP_IMAGE="ghcr.io/mahendraintelops/compage/app"+$TAG_NAME
-UI_IMAGE="ghcr.io/mahendraintelops/compage/ui"+$TAG_NAME
+CORE_IMAGE="ghcr.io/mahendraintelops/compage/core:$TAG_NAME"
+APP_IMAGE="ghcr.io/mahendraintelops/compage/app:$TAG_NAME"
+UI_IMAGE="ghcr.io/mahendraintelops/compage/ui:$TAG_NAME"
 
 # create docker images for core, app and ui
 docker build -t $CORE_IMAGE ../core/
@@ -25,18 +31,14 @@ docker build -t $APP_IMAGE ../app/
 docker build -t $UI_IMAGE ../ui/
 
 # load docker images for core, app and ui
-kind load docker-image $CORE_IMAGE
-kind load docker-image $APP_IMAGE
-kind load docker-image $UI_IMAGE
-
-# update KIND_NODE_IP in configmaps
-sed -i "s/{{KIND_NODE_IP}}/$KIND_NODE_IP/" ../app/manifests/configmaps.yaml
-sed -i "s/{{KIND_NODE_IP}}/$KIND_NODE_IP/" ../ui/manifests/configmaps.yaml
+kind load docker-image --name $CLUSTER_NAME $CORE_IMAGE
+kind load docker-image --name $CLUSTER_NAME $APP_IMAGE
+kind load docker-image --name $CLUSTER_NAME $UI_IMAGE
 
 # update image tags for core, app and ui
-sed -i "s/{{CORE_IMAGE}}/$CORE_IMAGE/" ../core/manifests/deployment.yaml
-sed -i "s/{{APP_IMAGE}}/$APP_IMAGE/" ../app/manifests/deployment.yaml
-sed -i "s/{{UI_IMAGE}}/$UI_IMAGE/" ../ui/manifests/deployment.yaml
+sed -i "s#{{CORE_IMAGE}}#$CORE_IMAGE#" ../core/manifests/deployment.yaml
+sed -i "s#{{APP_IMAGE}}#$APP_IMAGE#" ../app/manifests/deployment.yaml
+sed -i "s#{{UI_IMAGE}}#$UI_IMAGE#" ../ui/manifests/deployment.yaml
 
 #######################################################################
 # Make sure you have created docker images and updated the image tags #
@@ -50,4 +52,7 @@ kubectl apply -f ../app/manifests
 # creates resources for ui
 kubectl apply -f ../ui/manifests
 
-echo "Your KinD cluster has $KIND_NODE_IP as node "
+echo "Your KinD cluster has $KIND_NODE_IP as node ip, please check your github app has the same ip in Homepage URL and Authorization callback URL"
+
+echo "Your compage images have been loaded to cluster : $CLUSTER_NAME"
+docker exec -it ${CLUSTER_NAME}-control-plane crictl images | grep compage
