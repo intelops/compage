e2ca5ae1532e7377d3c5900c7e8ef210ac54060d
Mahendra <mahendra.b@intelops.dev>
2022-12-27 13:49:06 +0530

    added .then, .catch based impl to capture the error from backend

--- ui/src/components/projects/async-apis/createProject.ts
+++ ui/src/components/projects/async-apis/createProject.ts
@@ -6,31 +6,30 @@ import {toastr} from 'react-redux-toastr'
 export const createProjectAsync = createAsyncThunk<CreateProjectResponse, CreateProjectRequest, { rejectValue: CreateProjectError }>(
     'projects/createProject',
     async (createProjectRequest: CreateProjectRequest, thunkApi) => {
-        try {
-            const response = await createProject(createProjectRequest);
-            // Check if status is not okay:
+        return createProject(createProjectRequest).then(response => {
             if (response.status !== 201) {
-                console.log("Failed to create project. Received : " + response.status)
-                toastr.error('Failure : ' + createProjectRequest.displayName, "Failed to create project. Received : " + response.status);
+                const message = `Failed to create project.`;
+                const errorMessage = `Status: ${response.status}, Message: ${message}`;
+                console.log(errorMessage);
+                toastr.error(`Failure: ${createProjectRequest.displayName}`, errorMessage);
                 // Return the error message:
                 return thunkApi.rejectWithValue({
-                    message: "Failed to create project. Received : " + response.status
+                    message: errorMessage
                 });
             }
-            console.log("Successfully created project-" + createProjectRequest.displayName)
-            toastr.success('Success : ' + createProjectRequest.displayName, "Successfully created project : " + createProjectRequest.displayName);
-            return response.data;
-        } catch (e: any) {
-            console.log(JSON.parse(JSON.stringify(e)))
-            const statusCode = e.response.status // 400
-            const statusText = e.response.statusText // Bad Request
-            const message = e.response.data.message[0] // id should not be empty
-            console.log(`e : ${e}`)
-
-            toastr.error('Failure : ' + createProjectRequest.displayName, "Failed to create project. Received : " + JSON.parse(JSON.stringify(e)).status);
+            const message = `Successfully created project: ${createProjectRequest.displayName}`
+            console.log(message)
+            toastr.success(`Success: ${createProjectRequest.displayName}`, message);
+            return response.data
+        }).catch(e => {
+            const statusCode = e.response.status;
+            const message = JSON.parse(JSON.stringify(e.response.data)).message;
+            const errorMessage = `Status: ${statusCode}, Message: ${message}`;
+            console.log(errorMessage);
+            toastr.error(`Failure: ${createProjectRequest.displayName}`, errorMessage);
             return thunkApi.rejectWithValue({
-                message: "Failed to create project. Received :" + e
+                message: errorMessage
             });
-        }
+        })
     }
 );
