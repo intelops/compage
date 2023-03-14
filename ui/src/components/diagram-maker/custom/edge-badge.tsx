2665b10432b48268049945f8e8c56bac792ed0ee
Mahendra <mahendra.b@intelops.dev>
2023-03-14 12:48:52 +0530

    added name from user input to edge's displayName

--- ui/src/components/diagram-maker/custom/edge-badge.tsx
+++ ui/src/components/diagram-maker/custom/edge-badge.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {getModifiedState} from "../../../utils/localstorage-client";
 
 interface EdgeBadgeProps {
     id: string;
@@ -8,9 +9,18 @@ interface EdgeBadgeProps {
 
 export const EdgeBadge = (props: EdgeBadgeProps) => {
     const id = props.id.substring(0, 10);
+    let modifiedState = getModifiedState();
+    // add name given by user to this edge.
+    const name = JSON.parse(modifiedState)?.edges[id]?.consumerData?.name;
+    let displayName;
+    if (name) {
+        displayName = name;
+    } else {
+        displayName = id;
+    }
     return <React.Fragment>
         <div className="edgeBadge" onDoubleClick={props.handleDblClick}>
-            {id}
+            {displayName}
         </div>
     </React.Fragment>;
 };
