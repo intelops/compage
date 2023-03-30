16f2e21bb10a264db0ae3ef9c6a8f108cc8d05f6
azar-intelops <amohammed@appstekcorp.com>
2023-03-30 12:33:06 +0530

    feat : Resources are now updated to dynamic form

--- ui/src/components/diagram-maker/new-properties/modify-rest-resource.tsx
+++ ui/src/components/diagram-maker/new-properties/modify-rest-resource.tsx
@@ -2,14 +2,16 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import Divider from "@mui/material/Divider";
 import DialogContent from "@mui/material/DialogContent";
-import {Stack, TextareaAutosize} from "@mui/material";
+import { FormControl, InputLabel, ListSubheader, MenuItem, Select, Stack} from "@mui/material";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import React, {ChangeEvent} from "react";
 import {Resource} from "../models";
 import TextField from "@mui/material/TextField";
-import {isJsonString} from "../helper/utils";
 import {sanitizeString} from "../../../utils/backend-api";
+import { goDataTypes } from "../../../utils/constants";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 interface ModifyRestResourceProperties {
     isOpen: boolean;
@@ -19,11 +21,39 @@ interface ModifyRestResourceProperties {
     nodeId: string;
 }
 
+const dataTypesParser = (datatype:string)=>{
+    switch (datatype) {
+        case "int":
+            return goDataTypes["int"].map((type:string) => <MenuItem key={type} value={type} >{type}</MenuItem>)
+        case "float":
+            return goDataTypes["float"].map((type:string) => <MenuItem key={type} value={type} >{type}</MenuItem>)
+        case "complex":
+            return goDataTypes["complex"].map((type:string) => <MenuItem key={type} value={type} >{type}</MenuItem>)
+        case "bool":
+            return goDataTypes["bool"].map((type:string) => <MenuItem key={type} value={type} >{type}</MenuItem>)
+        case "string":
+            return goDataTypes["string"].map((type:string) => <MenuItem key={type} value={type} >{type}</MenuItem>)
+        default:
+            break;
+    }
+}
+
 export const ModifyRestResource = (props: ModifyRestResourceProperties) => {
     const [data, setData] = React.useState({
         name: props.resource?.name || "",
         fields: JSON.stringify(props.resource?.fields) || "",
     });
+
+    // individual states for Attribute Name and Data Type
+    const [field, setField] = React.useState({
+        attribute : "", 
+        datatype:""
+    })
+
+    // 
+
+    // Dynamic Collection for adding select fields
+    const [fieldCollection, setFieldCollection] = React.useState([{attribute: "", datatype: ""}])
 
     // first letter of the Resource should always be capital.
     const capitalizeFirstLetter = (input: string) => {
@@ -37,14 +67,30 @@ export const ModifyRestResource = (props: ModifyRestResourceProperties) => {
         });
     };
 
-    const handleFieldsChange = (event: ChangeEvent<HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement>) => {
-        setData({
-            ...data,
-            fields: event.target.value
-        });
+    const handleFieldsChange = () => {
+        let fieldsToJson = {};
+        if(field.attribute === "" || field.datatype === ""){
+            for (var i = 0; i < fieldCollection.length - 1; i++) {
+                fieldsToJson[fieldCollection[i].attribute] = fieldCollection[i].datatype;
+              }
+        }
+        else{
+            fieldCollection.filter((value)=> value.attribute !== "" && value.datatype !== "").forEach((value:any)=>fieldsToJson[value.attribute] = value.datatype)
+            if(field.attribute !== "" || field.datatype !== ""){
+                let temp = {}
+                temp[field.attribute] = field.datatype
+                fieldsToJson = {
+                    ...fieldsToJson,
+                    ...temp
+                }
+            }
+        }
+        return fieldsToJson;
     };
 
     const handleModifyRestResourceClick = () => {
+        let fieldsToJson = handleFieldsChange()
+        data.fields = JSON.stringify(fieldsToJson)
         const resource: Resource = {fields: JSON.parse(data.fields), name: data.name};
         props.handleModifyRestResourceClick(resource);
         setData({
@@ -52,6 +98,7 @@ export const ModifyRestResource = (props: ModifyRestResourceProperties) => {
             fields: "",
             name: ""
         });
+        setFieldCollection([{attribute: "", datatype: ""}])
     };
 
     const onClose = (e: any, reason: "backdropClick" | "escapeKeyDown") => {
@@ -62,6 +109,53 @@ export const ModifyRestResource = (props: ModifyRestResourceProperties) => {
         props.onModifyRestResourceClose();
     };
 
+
+    const handleAddField = (index:number)=>{
+        if(field.attribute && field.datatype){
+            fieldCollection[index].attribute = field.attribute
+            fieldCollection[index].datatype = field.datatype
+            setField({attribute:"",datatype:""})
+            setFieldCollection([...fieldCollection,{attribute:"",datatype:""}])
+        }
+    }
+
+    const handleRemoveField = (index:number)=>{
+        if(fieldCollection.length > 1){
+            setFieldCollection([...fieldCollection.slice(0, index),...fieldCollection.slice(index+1)])
+        }
+    }
+
+    const addButtonStatus = (index:number)=>{
+        if(field.attribute === "" && field.datatype === "" && fieldCollection[index].attribute === "" && fieldCollection[index].datatype === "" ){
+            return true
+        }
+        if(fieldCollection[index].attribute !== "" && fieldCollection[index].datatype !== ""){
+            return true
+        }
+        if(field.attribute !== "" || field.datatype !== "" ){
+            return false
+        }
+    }
+
+    const removeButtonStatus = (index:number)=>{
+        if((fieldCollection.length > 1) && fieldCollection[index].attribute !== "" && fieldCollection[index].datatype !== ""){
+            return false
+        }
+        return true
+    }
+
+    // Styles for select field in MUI
+    const ITEM_HEIGHT = 45;
+    const ITEM_PADDING_TOP = 4;
+    const MenuProps = {
+    PaperProps: {
+        style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 120,
+        },
+    },
+    };
+    
     return <React.Fragment>
         <Dialog open={props.isOpen} onClose={onClose}>
             <DialogTitle>Modify [REST Server] resource : {props.nodeId}</DialogTitle>
@@ -82,25 +176,82 @@ export const ModifyRestResource = (props: ModifyRestResourceProperties) => {
                         onChange={handleNameChange}
                         variant="outlined"
                     />
-                    <TextareaAutosize
-                        aria-label="fields"
-                        placeholder="Fields"
-                        maxRows={6}
-                        minRows={5}
-                        required
-                        id="fields"
-                        value={data.fields}
-                        onChange={handleFieldsChange}
-                    />
+                    <Stack direction="column" spacing={1}>
+                        {
+                            fieldCollection.map((fieldType, index)=>{
+                                return (
+                                    <Stack key={index} direction="row" alignItems="center" spacing={1}>
+                                        <TextField
+                                            required
+                                            size="medium"
+                                            margin="dense"
+                                            id="attribute"
+                                            label="Attribute"
+                                            type="text"
+                                            value={fieldType.attribute === "" ? field.attribute : fieldType.attribute }
+                                            onChange={(e) => {
+                                                setField({
+                                                ...field,
+                                                attribute:e.target.value
+                                            })}}
+                                            variant="outlined"
+                                        />
+                                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                                            <InputLabel id="field-select-input">Data Type</InputLabel>
+                                            <Select
+                                                labelId="field-select-input"
+                                                defaultValue=""
+                                                onChange={(e)=>{
+                                                    setField({
+                                                        ...field,
+                                                        datatype:e.target.value
+                                                    })
+                                                }}
+                                                id="grouped-select"
+                                                label="Data Type"
+                                                MenuProps={MenuProps}
+                                            >
+                                                <ListSubheader>int</ListSubheader>
+                                                {
+                                                    dataTypesParser("int")
+                                                }
+                                                <ListSubheader>float</ListSubheader>
+                                                {
+                                                    dataTypesParser("float")
+                                                }
+                                                <ListSubheader>complex</ListSubheader>
+                                                {
+                                                    dataTypesParser("complex")
+                                                }
+                                                <ListSubheader>bool</ListSubheader>
+                                                {
+                                                    dataTypesParser("bool")
+                                                }
+                                                <ListSubheader>string</ListSubheader>
+                                                {
+                                                    dataTypesParser("string")
+                                                }
+                                            </Select>
+                                        </FormControl>
+                                        <Stack direction="row">
+                                            <Button onClick={()=>handleAddField(index)} disabled={addButtonStatus(index)} >
+                                                <AddIcon/>
+                                            </Button>
+                                            <Button disabled={removeButtonStatus(index)} onClick={()=>handleRemoveField(index)}>
+                                                <RemoveIcon/>
+                                            </Button>
+                                        </Stack>
+                                    </Stack>
+                                )
+                            })
+                        }
+                    </Stack>
                 </Stack>
             </DialogContent>
             <DialogActions>
                 <Button variant="outlined" color="secondary" onClick={props.onModifyRestResourceClose}>Cancel</Button>
                 <Button variant="contained"
-                        disabled={data.name === ""
-                            || !isJsonString(data.fields)
-                            // TODO below is now working
-                            || Object.keys(data.fields).length === 0}
+                        disabled={data.name === ""}
                         onClick={handleModifyRestResourceClick}>
                     Modify Resource
                 </Button>
