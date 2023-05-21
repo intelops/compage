54005c2f6e065eb1fdd885e17b88a41fc5c5af81
Mahendra <mahendra.b@intelops.dev>
2023-05-21 18:22:08 +0530

    test: modified tests, added new for grpc

--- core/test/generator_test.go
+++ core/test/generator_test.go
@@ -1,48 +1,52 @@
 package test
 
 import (
-	"fmt"
 	"github.com/intelops/compage/core/gen/api/v1"
 	"github.com/intelops/compage/core/internal/converter/grpc"
 	"github.com/intelops/compage/core/internal/handlers"
+	"log"
+	"os"
 	"testing"
 )
 
-func TestGenerator(t *testing.T) {
-	jsonString := `{
-  "edges": {},
-  "nodes": {
-    "node-3d": {
-      "id": "node-3d",
-      "typeId": "node-type-circle",
-      "consumerData": {
-        "nodeType": "circle",
-        "name": "student-service",
-        "language": "go",
-        "restServerConfig": {
-          "framework": "go-gin-server",
-          "port": "3434",
-           "sqlDb":"MySQL",
-          "template": "compage",
-          "resources": [
-            { "fields": { "Name": "string", "City": "string","Age": "int64" }, "name": "Student" }
-          ]
+func TestRestGenerator(t *testing.T) {
+	restConfigJson := `{
+    "edges": {},
+    "nodes": {
+        "node-ef": {
+            "id": "node-ef",
+            "typeId": "node-type-circle",
+            "consumerData": {
+                "nodeType": "circle",
+                "name": "user-service",
+                "language": "go",
+                "restServerConfig": {
+                    "sqlDb": "SQLite",
+                    "framework": "go-gin-server",
+                    "port": "3000",
+                    "template": "compage",
+                    "resources": [
+                        {
+                            "fields": {
+                                "Name": "int8"
+                            },
+                            "name": "User"
+                        }
+                    ]
+                }
+            }
         }
-      }
     }
-  }
 }`
-	fmt.Println(jsonString)
-	grpcConfigJson := `{"edges":{},"nodes":{"node-b0":{"id":"node-b0","typeId":"node-type-circle","consumerData":{"nodeType":"circle","name":"student-service","language":"go","grpcServerConfig":{"template":"compage","sqlDb":"SQLite","framework":"go-grpc-server","port":"12224","resources":[{"fields":{"Name":"string","RollNumber":"int32","College":"string"},"name":"Student"}]}}}}}`
 	input := project.GenerateCodeRequest{
 		UserName:       "mahendraintelops",
 		RepositoryName: "first-project-github",
-		ProjectName:    "first-project",
-		Json:           grpcConfigJson,
+		ProjectName:    "first-rest-project",
+		Json:           restConfigJson,
 	}
-	//defer func() {
-	//	_ = os.RemoveAll("/tmp/first-project")
-	//}()
+	defer func() {
+		_ = os.RemoveAll("/tmp/first-rest-project")
+	}()
 
 	// retrieve project struct
 	getProject, err := grpc.GetProject(&input)
@@ -51,6 +55,60 @@ func TestGenerator(t *testing.T) {
 	}
 	// trigger project generation
 	if err0 := handlers.Handle(getProject); err0 != nil {
+		log.Fatal(err0)
+		t.Errorf("handlers.Handle failed %s", err0.Error())
+	}
+}
+
+func TestGrpcGenerator(t *testing.T) {
+	grpcConfigJson := `{
+    "edges": {},
+    "nodes": {
+        "node-b0": {
+            "id": "node-b0",
+            "typeId": "node-type-circle",
+            "consumerData": {
+                "nodeType": "circle",
+                "name": "student-service",
+                "language": "go",
+                "grpcServerConfig": {
+                    "template": "compage",
+                    "sqlDb": "SQLite",
+                    "framework": "go-grpc-server",
+                    "port": "50051",
+                    "resources": [
+                        {
+                            "fields": {
+                                "Name": "string",
+                                "RollNumber": "int32",
+                                "College": "string"
+                            },
+                            "name": "Student"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+}`
+	input := project.GenerateCodeRequest{
+		UserName:       "mahendraintelops",
+		RepositoryName: "first-project-github",
+		ProjectName:    "first-grpc-project",
+		Json:           grpcConfigJson,
+	}
+	defer func() {
+		_ = os.RemoveAll("/tmp/first-grpc-project")
+	}()
+
+	// retrieve project struct
+	getProject, err := grpc.GetProject(&input)
+	if err != nil {
+		t.Errorf("grpc.GetProject conversion failed = %v", getProject)
+	}
+	// trigger project generation
+	if err0 := handlers.Handle(getProject); err0 != nil {
+		log.Fatal(err0)
 		t.Errorf("handlers.Handle failed %s", err0.Error())
 	}
 }
